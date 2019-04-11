ce6e18a65d8c64a5853e37d0e1b6e475430fb439
John Grando <grandocu@gmail.com>
2019-04-11 19:02:04 -0400

    updating nn runs for full test sets

--- RScripts/ngbtu_nn_hyperparameter_train_psf.R
+++ RScripts/ngbtu_nn_hyperparameter_train_psf.R
@@ -1,7 +1,7 @@
 #Set up base and compile
 source('RScripts/ngbtu_nn_base_model_psf.R')
 #Get model types
-source('RScripts/elbtu_nn_model_functions_build.R')
+source('RScripts/ngbtu_nn_model_functions_build.R')
 
 #if hyperparameter df exists then use it
 if(head_object('ModelSaves/ngbtu_nn_hyperparameter_results_psf.RData', bucket='cuny-msds-final-project-cbecs')[1]==TRUE){
@@ -17,33 +17,33 @@ set.seed(20)
 folds <- createFolds(y = train_test_list, k=n_folds, list=FALSE)
 
 hyper_list <- list()
-hyper_list$dropout <- c(0.85, 0.9, 0.95, 0.99)
-hyper_list$units <- seq(200, 600, 100)
-hyper_list$regularizer <- seq(0, 0.3, 0.05)
+hyper_list$dropout <- seq(0.3, 0.9, 0.3)
+hyper_list$units <- seq(200, 600, 200)
+hyper_list$regularizer <- seq(0, 0.3, 0.15)
 hyper_list$model <- seq(3,5,1)
-hyper_list$batch <- append(c(10, 25), seq(50, 250, 100))
+hyper_list$batch <- seq(150, 350, 100)
 hyper_list$loss <- list(
-      #list(name = 'mse', func = 'mse'), 
-      list(name = 'msle', func = keras::loss_mean_squared_logarithmic_error)#,
-      #list(name = 'cusom_loss_func', func = custom_loss_func)
+  #list(name = 'mse', func = 'mse'), 
+  list(name = 'msle', func = keras::loss_mean_squared_logarithmic_error),
+  list(name = 'cusom_loss_func', func = custom_loss_func)
 )
 hyper_list$opt <- list(
   #list(name = 'rmsprop_lr_00005', func = keras::optimizer_rmsprop(lr = 0.00005)),
   list(name = 'rmsprop_lr_0001', func = keras::optimizer_rmsprop(lr = 0.0001)),
   list(name = 'rmsprop_lr_0005', func = keras::optimizer_rmsprop(lr = 0.0005)),
-  list(name = 'rmsprop_lr_001', func = keras::optimizer_rmsprop(lr = 0.001))#,
-  #list(name = 'sgd_lr_005_m_0_d_0_n_T', func = keras::optimizer_sgd(lr=0.005, momentum=0.0, decay=0.0, nesterov=TRUE)),
+  list(name = 'rmsprop_lr_001', func = keras::optimizer_rmsprop(lr = 0.001)),
+  list(name = 'sgd_lr_005_m_0_d_0_n_T', func = keras::optimizer_sgd(lr=0.005, momentum=0.0, decay=0.0, nesterov=TRUE)),
   #list(name = 'sgd_lr_01_m_0_d_0_n_T', func = keras::optimizer_sgd(lr=0.01, momentum=0.0, decay=0.0, nesterov=TRUE)),
   #list(name = 'sgd_lr_015_m_0_d_0_n_T', func = keras::optimizer_sgd(lr=0.015, momentum=0.0, decay=0.0, nesterov=TRUE)),
-  #list(name = 'sgd_lr_005_m_0_d_0_n_F', func = keras::optimizer_sgd(lr=0.005, momentum=0.0, decay=0.0, nesterov=FALSE)),
+  list(name = 'sgd_lr_005_m_0_d_0_n_F', func = keras::optimizer_sgd(lr=0.005, momentum=0.0, decay=0.0, nesterov=FALSE)),
   #list(name = 'sgd_lr_01_m_5_d_0_n_T', func = keras::optimizer_sgd(lr=0.01, momentum=0.5, decay=0.0, nesterov=TRUE)),
   #list(name = 'sgd_lr_015_m_9_d_0_n_T', func = keras::optimizer_sgd(lr=0.015, momentum=0.9, decay=0.0, nesterov=TRUE)),
   #list(name = 'sgd_lr_005_m_0_d_1E6_n_T', func = keras::optimizer_sgd(lr=0.005, momentum=0.0, decay=1e-6, nesterov=TRUE)),
-  #list(name = 'sgd_lr_01_m_5_d_1E3_n_T', func = keras::optimizer_sgd(lr=0.01, momentum=0.5, decay=1e-3, nesterov=TRUE)),
+  list(name = 'sgd_lr_01_m_5_d_1E3_n_T', func = keras::optimizer_sgd(lr=0.01, momentum=0.5, decay=1e-3, nesterov=TRUE)),
   #list(name = 'sgd_lr_015_m_9_d_1_n_T', func = keras::optimizer_sgd(lr=0.015, momentum=0.9, decay=0.1, nesterov=TRUE)),
   #list(name = 'sgd_lr_005_m_0_d_1E6_n_T', func = keras::optimizer_sgd(lr=0.005, momentum=0.0, decay=1e-6, nesterov=TRUE)),
   #list(name = 'sgd_lr_01_m_0_d_1E3_n_T', func = keras::optimizer_sgd(lr=0.01, momentum=0.0, decay=1e-3, nesterov=TRUE)),
-  #list(name = 'sgd_lr_015_m_0_d_1_n_T', func = keras::optimizer_sgd(lr=0.015, momentum=0.0, decay=0.1, nesterov=TRUE))
+  list(name = 'sgd_lr_015_m_0_d_1_n_T', func = keras::optimizer_sgd(lr=0.015, momentum=0.0, decay=0.1, nesterov=TRUE))
 )
 
 #Initialize parallel processing on 2 cores
@@ -52,27 +52,27 @@ registerDoMC(3)
 
 #Run model
 epochs <- 100
-for(v in c(#seq(0,length(variables_by_importance),300), 
-           length(variables_by_importance)-100,
-           length(variables_by_importance)-50,
-           length(variables_by_importance)-20,
-           length(variables_by_importance)-10#,
-           #length(variables_by_importance)-1
-           )) {
-  for (o in 1:length(hyper_list$opt)) {
-    for (l in 1:length(hyper_list$loss)){
-      for (m in hyper_list$model) {
-        for (r in hyper_list$regularizer) {
-          for (d in hyper_list$dropout) {
-            for (u in hyper_list$units) {
-              for (b in hyper_list$batch) {
+
+for (o in 1:length(hyper_list$opt)) {
+  for (l in 1:length(hyper_list$loss)){
+    for (m in hyper_list$model) {
+      for (r in hyper_list$regularizer) {
+        for (d in hyper_list$dropout) {
+          for (u in hyper_list$units) {
+            for (b in hyper_list$batch) {
+              for(v in c(seq(0,length(variables_by_importance),300), 
+                         length(variables_by_importance)-100,
+                         length(variables_by_importance)-50,
+                         length(variables_by_importance)-10,
+                         length(variables_by_importance)-2
+              )) {
                 tmp_df <- foreach(f = 1:n_folds,
                                   .combine = rbind) %dopar% {
                                     #print(paste('running dropout, units, regularizer, fold:',d,u,r,f, sep=' '))
                                     ind <- which(folds == f)
                                     cv_train_df <- train_test_df %>% 
                                       slice(-ind) %>% 
-                                      select(-PBA, -NGBTUPerSf) %>% 
+                                      select(-PBA, -ngbtuPerSf) %>% 
                                       select(variables_by_importance[1:(length(variables_by_importance)-v)])
                                     cv_train_y <- train_test_df %>% 
                                       slice(-ind) %>% 
@@ -96,8 +96,8 @@ for(v in c(#seq(0,length(variables_by_importance),300),
                                       metrics = list("mean_absolute_error", 
                                                      "mean_squared_error", 
                                                      'mean_squared_logarithmic_error',
-                                                     percentage_metric
-                                                     ))
+                                                     mean_absolute_percentage_error
+                                      ))
                                     history <- cv_model_t %>% fit(
                                       as.matrix(cv_train_df),
                                       as.matrix(cv_train_y),
@@ -108,8 +108,8 @@ for(v in c(#seq(0,length(variables_by_importance),300),
                                     loss_val <- tail(history$metrics$val_loss,1)
                                     mae_val <- tail(history$metrics$val_mean_absolute_error,1)
                                     msle_val <- tail(history$metrics$val_mean_squared_logarithmic_error,1)
-				                            mse_val <- tail(history$metrics$val_mean_squared_error,1)
-                                    pm_val <- tail(history$metrics$percentage_metric,1)
+                                    mse_val <- tail(history$metrics$val_mean_squared_error,1)
+                                    mape_val <- tail(history$metrics$mean_absolute_percentage_error,1)
                                     #hyper_results <- rbind(hyper_results, data.frame(dropout=d, units=u, fold=f, loss= loss_val, mae = mae_val, pm = pm_val))
                                     data.frame(num_vars=length(variables_by_importance)-v,
                                                loss_f=hyper_list$loss[[l]]$name,
@@ -123,9 +123,9 @@ for(v in c(#seq(0,length(variables_by_importance),300),
                                                loss= loss_val, 
                                                mae = mae_val, 
                                                msle = msle_val,
-					                                     mse = mse_val,
-                                               pm = pm_val,
-					                                     run_time = Sys.time())
+                                               mse = mse_val,
+                                               mape = mape_val,
+                                               run_time = Sys.time())
                                   }
                 print(paste('finished running model - variables:', length(variables_by_importance)-v,
                             'loss:', hyper_list$loss[[l]]$name, 
@@ -137,29 +137,29 @@ for(v in c(#seq(0,length(variables_by_importance),300),
                             'batch:', b, sep = " "))
                 hyper_results <- rbind(hyper_results, tmp_df)
                 print(hyper_results %>% 
-                      group_by(num_vars, loss_f, opt, model, dropout, units, reg, batch) %>% 
-                      summarize(mae = mean(mae), 
-				                        mse = mean(mse),
-				                        msle = mean(msle),
-				                        loss = mean(loss)
-				                        #pm = mean(pm)
-				                        )%>% 
-                      arrange(msle) %>% 
-                      head(10))
+                        group_by(num_vars, loss_f, opt, model, dropout, units, reg, batch) %>% 
+                        summarize(mae = mean(mae), 
+                                  mape = mean(mape),
+                                  mse = mean(mse),
+                                  msle = mean(msle),
+                                  loss = mean(loss)
+                        )%>% 
+                        arrange(mape) %>% 
+                        head(10))
               }
             }
           }
         }
       }
     }
+    print('intermittent print to df')
+    #save hyperparameter df
+    model_name <- 'ModelSaves/ngbtu_nn_hyperparameter_results_psf.RData'
+    save(hyper_results, file = model_name)
+    put_object(file = model_name, 
+               bucket = 'cuny-msds-final-project-cbecs', 
+               object = model_name)
   }
-  print('intermittent print to df')
-  #save hyperparameter df
-  model_name <- 'ModelSaves/ngbtu_nn_hyperparameter_results_psf.RData'
-  save(hyper_results, file = model_name)
-  put_object(file = model_name, 
-             bucket = 'cuny-msds-final-project-cbecs', 
-             object = model_name)
 }
 #save hyperparameter df
 model_name <- 'ModelSaves/ngbtu_nn_hyperparameter_results_psf.RData'
