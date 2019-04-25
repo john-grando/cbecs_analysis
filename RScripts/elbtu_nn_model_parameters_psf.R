0ac89c6a8eb48779f11b799980dc0180945c321e
jgrando <john.grando@gmail.com>
2019-04-24 22:23:17 -0400

    updating report

--- RScripts/elbtu_nn_model_parameters_psf.R
+++ RScripts/elbtu_nn_model_parameters_psf.R
@@ -1,6 +1,6 @@
 #Select model parameters
 model <- model_selector(model_n = '5', df = train_reduced_df, n_dropout=0.6, n_units=600, n_l = 0)
-batch_size <- 150
+batch_size <- 250
 optimizer_func <- keras::optimizer_rmsprop(lr=0.001)
 #loss_func <- keras::loss_mean_squared_logarithmic_error
 loss_func <- custom_loss_func
