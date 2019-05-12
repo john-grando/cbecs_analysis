6bb79c3c7016e601587c46c7079adaeda20d135a
jgrando <john.grando@gmail.com>
2019-05-11 21:11:33 -0400

    updated ng models

--- RScripts/ngbtu_nn_model_parameters.R
+++ RScripts/ngbtu_nn_model_parameters.R
@@ -1,5 +1,5 @@
 #Select model parameters
-model <- model_selector(model_n = '3', df = train_reduced_df, n_dropout=0.9, n_units=400, n_l = 0)
-batch_size <- 50
-optimizer_func <- keras::optimizer_rmsprop(lr=0.001)
+model <- model_selector(model_n = '4', df = train_reduced_df, n_dropout=0.3, n_units=800, n_l = 0)
+batch_size <- 150
+optimizer_func <- keras::optimizer_rmsprop(lr=0.0001)
 loss_func <- keras::loss_mean_squared_logarithmic_error
